0cef24cc691d04439ba542fb89dea5d18c6804fc
Polar stoat <polarstoat@users.noreply.github.com>
2018-05-17 13:47:26 +0100

    Add basic crop profit calculation logic

--- app/js/main.js
+++ app/js/main.js
@@ -38,7 +38,145 @@ function getJSON(url) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  // do work
-  getJSON('js/crops.json').then(console.log);
-});
+function init() {
+  const SEASONS = ['spring', 'summer', 'fall', 'winter'];
+  const SEASON_LENGTH = 28;
+
+  const crops = [];
+  const date = {
+    day: 1,
+    season: 'spring',
+    year: 1,
+    timestamp: 0,
+  };
+
+  function cheapestSeedPrice(seed) {
+    let cheapest = Infinity;
+
+    Object.keys(seed.vendor).forEach((vendor, i, vendors) => {
+      // If the seed is only available from the Traveling Cart
+      if (vendors.length === 1 && vendor === 'travelingCart') {
+        // If the seed has a normal price, use that
+        if (typeof seed.vendor[vendor].price !== 'undefined') cheapest = seed.vendor[vendor].price;
+        // Otherwise use the average (already calculated in parseCropData) of the min and max price
+        else cheapest = seed.vendor[vendor].avgPrice;
+      } else if (seed.vendor[vendor].price < cheapest) cheapest = seed.vendor[vendor].price;
+    });
+
+    return cheapest;
+  }
+
+  function update() {
+    const cultivatableCrops = [];
+
+    crops.forEach((crop) => {
+      const cleanCrop = crop;
+
+      const dayOfYear = date.timestamp % (SEASON_LENGTH * 4);
+
+      if (dayOfYear < crop.seasonStartDate
+        || dayOfYear + crop.daysToGrow >= crop.seasonEndDate) return;
+
+      // TODO: Add 'Agriculturist' profession multiplier to crop.daysToGrow
+      const harvests = (crop.daysToRegrow)
+        ? Math.ceil((crop.seasonEndDate - dayOfYear - crop.daysToGrow) / crop.daysToRegrow)
+        : 1;
+
+      // TODO: Add chance for extra crops multiplier
+      // TODO: Add 'Tiller' profession crop value multiplier
+      const revenue = (crop.harvest.minHarvest)
+        ? crop.sellPrice * harvests * crop.harvest.minHarvest
+        : crop.sellPrice * harvests;
+      const profit = revenue - cheapestSeedPrice(crop.seed);
+      const avgProfit = profit / (((harvests - 1) * crop.daysToRegrow) + crop.daysToGrow);
+
+      cleanCrop.avgProfit = avgProfit;
+
+      cultivatableCrops.push(cleanCrop);
+    });
+
+    // Sort crops alphabetically
+    cultivatableCrops.sort((a, b) => a.name > b.name);
+
+    // Update DOM
+    const tbody = document.getElementById('results').children[1];
+    tbody.innerHTML = '';
+
+    cultivatableCrops.forEach((crop) => {
+      const tr = document.createElement('tr');
+      tr.innerHTML = `<th scope="row">${crop.name}</th><td>${crop.avgProfit.toFixed(2)}</td>`;
+      tbody.appendChild(tr);
+    });
+  }
+
+  /**
+   * Converts a season to it's corresponding integer, e.g. 'spring' => 0, 'summer' => 1, etc.
+   * Throws if an invalid season is input.
+   * @param  {string} str The season to convert
+   * @return {number}     Integer representation of the season
+   */
+  function seasonToInt(str) {
+    const index = SEASONS.indexOf(str);
+    if (index >= 0) return index;
+    throw new Error(`Failed to convert season '${str}' to integer`);
+  }
+
+  function parseCropData(cropData) {
+    // These are in crops.json but not a directly plantable crop, so ignored
+    const CROPS_TO_IGNORE = ['Wild Horseradish', 'Spice Berry', 'Common Mushroom', 'Winter Root'];
+
+    Object.values(cropData).forEach((crop) => {
+      const cleanCrop = crop;
+
+      if (CROPS_TO_IGNORE.indexOf(crop.name) > -1) return;
+
+      cleanCrop.daysToGrow = crop.phaseDays.reduce((a, b) => a + b, 0);
+      cleanCrop.daysToRegrow = (crop.regrowAfterHarvest === -1) ? 0 : crop.regrowAfterHarvest;
+
+      cleanCrop.seasonStartDate = seasonToInt(crop.seasonsToGrowIn[0]) * SEASON_LENGTH;
+      cleanCrop.seasonEndDate =
+        cleanCrop.seasonStartDate + (crop.seasonsToGrowIn.length * SEASON_LENGTH);
+
+      const tc = crop.seed.vendor.travelingCart;
+      if (tc) cleanCrop.seed.vendor.travelingCart.avgPrice = (tc.minPrice + tc.maxPrice) / 2;
+
+      crops.push(cleanCrop);
+    });
+
+    update();
+  }
+
+  function dateChanged(evt) {
+    const element = evt.target;
+    const { id } = element;
+
+    // TODO: Input sanitisation
+    if (id === 'day' || id === 'year') {
+      date[id] = Number(element.value);
+    } else if (id === 'season') {
+      date.season = element.value;
+    } else {
+      throw new Error();
+    }
+
+    date.timestamp =
+      ((date.year - 1) * (SEASON_LENGTH * 4))
+      + (seasonToInt(date.season) * SEASON_LENGTH)
+      + (date.day - 1);
+
+    /**
+     * If the crop data has been loaded and parsed, then update the display.
+     * If it hasn't, once it has loaded an itial update wil be called anyway.
+     */
+    if (crops.length) update();
+  }
+
+  // Add event listeners to date inputs
+  document.getElementById('day').addEventListener('input', dateChanged);
+  document.getElementById('season').addEventListener('change', dateChanged);
+  document.getElementById('year').addEventListener('input', dateChanged);
+
+  getJSON('js/crops.json').then(parseCropData);
+}
+
+document.addEventListener('DOMContentLoaded', init);

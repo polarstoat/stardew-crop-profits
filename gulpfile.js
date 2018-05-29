3a089383e417a3b9dc8447542fa5c061f204ff79
Polar stoat <polarstoat@users.noreply.github.com>
2018-05-29 16:09:19 +0100

    Update build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,89 @@
-// Largely taken from https://browsersync.io/docs/gulp#gulp-sass-css
+/*
+ * This gulpfile.js was based upon:
+ * https://browsersync.io/docs/gulp#gulp-sass-css
+ * https://github.com/yeoman/generator-webapp/blob/v3.0.1/app/templates/gulpfile.js
+ */
 
-// gulp + plugins
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-
-// browser-sync
+const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
+const del = require('del');
+const runSequence = require('run-sequence');
+const ghPages = require('gh-pages');
 
-gulp.task('sass', () => gulp.src('app/scss/*.scss')
-  .pipe(sass({ outputStyle: 'compressed' }))
-  .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-  .pipe(gulp.dest('app/css'))
+const $ = gulpLoadPlugins();
+
+let dev = true;
+
+gulp.task('styles', () => gulp.src('app/scss/*.scss')
+  .pipe($.plumber())
+  .pipe($.if(dev, $.sourcemaps.init()))
+  .pipe($.sass({
+    outputStyle: dev ? 'expanded' : 'compressed',
+  }).on('error', $.sass.logError))
+  .pipe($.autoprefixer({
+    browsers: ['last 2 versions'],
+  }))
+  .pipe($.if(dev, $.sourcemaps.write('.')))
+  .pipe(gulp.dest('dist/css'))
   .pipe(browserSync.stream()));
 
-gulp.task('serve', ['sass'], () => {
+gulp.task('scripts', () => gulp.src('app/js/*.js')
+  .pipe($.plumber())
+  .pipe($.if(!dev, $.uglifyEs.default()))
+  .pipe(gulp.dest('dist/js'))
+  .pipe(browserSync.stream()));
+
+gulp.task('json', () => gulp.src('app/js/*.json')
+  .pipe($.plumber())
+  .pipe($.if(!dev, $.jsonminify()))
+  .pipe(gulp.dest('dist/js')));
+
+gulp.task('images', () => gulp.src('app/img/**/*')
+  .pipe($.plumber())
+  .pipe($.if(!dev, $.imagemin()))
+  .pipe(gulp.dest('dist/img')));
+
+gulp.task('html', () => gulp.src('app/*.html')
+  .pipe($.plumber())
+  .pipe($.if(!dev, $.htmlmin({
+    collapseWhitespace: true,
+  })))
+  .pipe(gulp.dest('dist')));
+
+gulp.task('clean', () => del('dist'));
+
+gulp.task('build', (cb) => {
+  runSequence('clean', ['styles', 'scripts', 'json', 'images', 'html'], () => {
+    cb();
+  });
+});
+
+gulp.task('serve', ['build'], () => {
   browserSync.init({
-    server: 'app',
+    server: 'dist',
   });
 
-  gulp.watch('app/scss/*.scss', ['sass']);
-  gulp.watch('app/js/*.js').on('change', browserSync.reload);
-  gulp.watch('app/*.html').on('change', browserSync.reload);
+  gulp.watch([
+    'app/*.html',
+    'app/img/**/*',
+    'app/js/*.json',
+  ]).on('change', browserSync.reload);
+
+  gulp.watch('app/scss/*.scss', ['styles']);
+  gulp.watch('app/js/*.js', ['scripts']);
+});
+
+gulp.task('deploy', (callback) => {
+  dev = false;
+
+  runSequence('build', () => {
+    ghPages.publish('dist', (err) => {
+      if (err) throw console.error(err);
+
+      callback();
+    });
+  });
 });
 
 gulp.task('default', ['serve']);
